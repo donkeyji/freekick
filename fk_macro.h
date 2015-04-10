c51bb8929440e9828a44e29e95a73086ed53b47a
donkeyji <donkeyji@yahoo.com>
2015-04-10 22:02:50 +0800

    formatted

--- fk_macro.h
+++ fk_macro.h
@@ -1,9 +1,6 @@
 #ifndef _FK_TYPES_H_
 #define _FK_TYPES_H_
 
-/* exit code*/
-#define FK_EXIT_ERROR 1
-
 /*log level*/
 #define FK_LOG_ERROR 	0
 #define FK_LOG_WARNING 	1
@@ -19,6 +16,7 @@
 /*saved fd*/
 #define FK_SAVED_FD 16
 
+/*default setting*/
 #define FK_MAX_CONN 10240
 #define FK_LOG_PATH "/tmp/freekick.log"
 #define FK_PID_PATH "/tmp/freekick.pid"
@@ -27,9 +25,9 @@
 #define FK_DAEMON 1
 #define FK_NODAEMON 0
 #define FK_SVR_ADDR "0.0.0.0"
+#define FK_DB_CNT 8
 
 /*timeout of conn, unit: second*/
 #define FK_CLIENT_TIMEOUT 300
 
-#define FK_DB_CNT 16
 #endif
