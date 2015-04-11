4d736c8c4f491de82ec0ca85a2a7508ac7b9eca0
donkeyji <donkeyji@yahoo.com>
2015-04-11 16:15:25 +0800

    poll ok

--- fk_poll.c
+++ fk_poll.c
@@ -2,7 +2,7 @@
 
 typedef struct _fk_poll {
 	struct pollfd *evlist;
-	size_t idx;
+	int last;
 	int *pidx;
 } fk_poll;
 
@@ -25,20 +25,27 @@ void *fk_poll_create(int max_conn)
 	iompx = (fk_poll *)fk_mem_alloc(sizeof(fk_poll));
 	iompx->evlist = (struct pollfd *)fk_mem_alloc(sizeof(struct pollfd) * (max_conn + 1 + FK_SAVED_FD));
 	bzero(iompx->evlist, sizeof(struct pollfd) * (max_conn + 1 + FK_SAVED_FD));
+	iompx->last = 0;
 	iompx->pidx = (int *)fk_mem_alloc(sizeof(int) * (max_conn + 1 + FK_SAVED_FD));
-	iompx->idx = 0;
+	memset(iompx->pidx, -1, sizeof(int));//initialize to -1
 
 	return iompx;
 }
 
 int fk_poll_add(void *ev_iompx, int fd, unsigned char type)
 {
+	int idx;
 	short ev;
 	fk_poll *iompx;
 	struct pollfd *pfd;
 
 	iompx = (fk_poll *)ev_iompx;
-	pfd = iompx->evlist + iompx->idx;
+
+	idx = iompx->pidx[fd];
+	if (idx == -1) {//never add this fd
+		idx =  iompx->last;
+	}
+	pfd = iompx->evlist + idx;
 
 	ev = 0x0000;
 	if (type & FK_EV_READ) {
@@ -47,23 +54,32 @@ int fk_poll_add(void *ev_iompx, int fd, unsigned char type)
 	if (type & FK_EV_WRITE) {
 		ev |= POLLOUT;
 	}
-	pfd->events = ev;
+	pfd->events |= ev;
 	pfd->fd = fd;
-	iompx->pidx[fd] = iompx->idx;
+	iompx->pidx[fd] = idx;
+	if (idx == iompx->last) {//a new fd
+		iompx->last++;
+		fk_log_debug("after add: last %d\n", iompx->last);
+	}
 
-	iompx->idx++;
-	fk_log_debug("add poll: fd %d\n", fd);
 	return 0;
 }
 
 int fk_poll_remove(void *ev_iompx, int fd, unsigned char type)
 {
+	int idx;
 	short ev;
 	fk_poll *iompx;
 	struct pollfd *pfd;
 
 	iompx = (fk_poll *)ev_iompx;
-	pfd = iompx->evlist + fd;
+
+	idx = iompx->pidx[fd];
+	if (idx == -1) {//never add this fd
+		fk_log_warn("try to remove a non-existing ev\n");
+		return -1;
+	}
+	pfd = iompx->evlist + idx;
 
 	ev = 0x000;
 	if (type & FK_EV_READ) {
@@ -73,9 +89,12 @@ int fk_poll_remove(void *ev_iompx, int fd, unsigned char type)
 		ev |= POLLOUT;
 	}
 	pfd->events &= (~ev);
-	if (pfd->events == 0x000) {
+	if (pfd->events == 0x000) {//need to delete
 		pfd->fd = 0;
 		pfd->revents = 0;
+		memcpy(pfd, iompx->evlist + iompx->last - 1, sizeof(struct pollfd));
+		bzero(iompx->evlist + iompx->last - 1, sizeof(struct pollfd));
+		iompx->last--;
 	}
 
 	return 0;
@@ -97,16 +116,14 @@ int fk_poll_dispatch(void *ev_iompx, struct timeval *timeout)
 	iompx = (fk_poll *)ev_iompx;
 
 	fk_log_debug("to poll\n");
-	nfds = poll(iompx->evlist, iompx->idx, ms_timeout);
+	fk_log_debug("---last: %d\n", iompx->last);
+	nfds = poll(iompx->evlist, iompx->last, ms_timeout);
 	if (nfds < 0) {
 		fk_log_debug("poll return: %s\n", strerror(errno));
 		return -1;
 	}
 	for (i = 0; i < nfds; i++) {
 		pfd = iompx->evlist + i;
-		if (pfd == NULL) {
-			continue;
-		}
 		fd = pfd->fd;
 		type = 0x00;
 		if (pfd->revents & POLLIN) {
