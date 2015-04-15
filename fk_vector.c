38de960cc760878208cec5d55fba6635b9526151
donkeyji <donkeyji@yahoo.com>
2015-04-15 10:10:48 +0800

    vector ok

---
+++ fk_vector.c
@@ -0,0 +1,27 @@
+#include <strings.h>
+
+#include <fk_mem.h>
+#include <fk_vector.h>
+
+
+fk_vector *fk_vector_create()
+{
+	fk_vector *vtr;
+
+	vtr = (fk_vector *)fk_mem_alloc(sizeof(fk_vector));
+	if (vtr == NULL) {
+		return NULL;
+	}
+	vtr->array = (fk_str **)fk_mem_alloc(sizeof(fk_str *) * FK_VECTOR_INIT_LEN);
+	if (vtr->array == NULL) {
+		fk_mem_free(vtr);
+		return NULL;
+	}
+	vtr->len = FK_VECTOR_INIT_LEN;
+	bzero(vtr->array, vtr->len * sizeof(fk_str *));
+	return vtr;
+}
+
+void fk_vector_destroy(fk_vector *vtr)
+{
+}
---
+++ fk_vector.h
@@ -0,0 +1,37 @@
+#ifndef _FK_VECTOR_H_
+#define _FK_VECTOR_H_
+
+#include <fk_str.h>
+
+#define FK_VECTOR_INIT_LEN	4
+
+typedef struct _fk_vector {
+	int len;
+	fk_str **array;
+} fk_vector;
+
+fk_vector *fk_vector_create();
+void fk_vector_destroy(fk_vector *vtr);
+
+#define FK_VECTOR_RAW(vtr)		(vtr)->array
+
+#define FK_VECTOR_LEN(vtr)		(vtr)->len
+
+#define FK_VECTOR_STRETCH(vtr, length)	{							\
+	if ((length) > (vtr)->len) {									\
+		(vtr)->array = (fk_str **)fk_mem_realloc((vtr)->array, 		\
+				(sizeof(fk_str *) * (length)));						\
+		bzero((vtr)->array + (vtr)->len, (length) - (vtr)->len);	\
+		(vtr)->len = (length);										\
+	}																\
+}
+
+#define FK_VECTOR_SHRINK(vtr)	{								\
+	if ((vtr)->len > FK_VECTOR_INIT_LEN) {						\
+		(vtr)->array = (fk_str **)fk_mem_realloc((vtr)->array, 	\
+				sizeof(fk_str *) * FK_VECTOR_INIT_LEN);			\
+		(vtr)->len = FK_VECTOR_INIT_LEN;						\
+	}															\
+}
+
+#endif
