da52c78879b92b7dffc76e7eebcf8a6c101755b8
donkeyji <donkeyji@yahoo.com>
2015-04-18 22:51:00 +0800

    path modify

--- archive.sh
+++ archive.sh
@@ -2,4 +2,5 @@
 
 d=$(date "+%Y-%m-%d")
 name="hopex-$d.tar.gz"
-git archive master --prefix='hopex/' | gzip > ~/Work/VShared/freekick/code/hope-release/$name
+dst_dir="/Users/huge/Work/VShared/freekick/code/release-hopex"
+git archive master --prefix='hopex/' | gzip > $dst_dir/$name
