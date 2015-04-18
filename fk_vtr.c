b1b7e6c746f5a1260f28719fe05c1db536f687f4
donkeyji <donkeyji@yahoo.com>
2015-04-18 21:48:10 +0800

    remove fk_mem_alloc NULL check

--- fk_vtr.c
+++ fk_vtr.c
@@ -10,14 +10,7 @@ fk_vtr *fk_vtr_create()
 	fk_vtr *vtr;
 
 	vtr = (fk_vtr *)fk_mem_alloc(sizeof(fk_vtr));
-	if (vtr == NULL) {
-		return NULL;
-	}
 	vtr->array = (fk_str **)fk_mem_alloc(sizeof(fk_str *) * FK_VTR_INIT_LEN);
-	if (vtr->array == NULL) {
-		fk_mem_free(vtr);
-		return NULL;
-	}
 	vtr->len = FK_VTR_INIT_LEN;
 	bzero(vtr->array, vtr->len * sizeof(fk_str *));
 	return vtr;
