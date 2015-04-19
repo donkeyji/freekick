d2a3325b0dbfa2b654de09792dc5f252a8457c51
donkeyji <donkeyji@yahoo.com>
2015-04-19 15:27:39 +0800

    modify archive.sh

--- archive.sh
+++ archive.sh
@@ -4,3 +4,13 @@ d=$(date "+%Y-%m-%d")
 name="hopex-$d.tar.gz"
 dst_dir="/Users/huge/Work/VShared/freekick/code/release-hopex"
 git archive master --prefix='hopex/' | gzip > $dst_dir/$name
+
+bak_dir="/Volumes/SD CARD/X-STARS/hopex/"
+cd "$bak_dir"
+git pull
+bak_dir="/Volumes/NO NAME/Z-Music/hopex/"
+cd "$bak_dir"
+git pull
+bak_dir="/Volumes/Macintosh/Users/huge/Pictures/hopex/"
+cd "$bak_dir"
+git pull
