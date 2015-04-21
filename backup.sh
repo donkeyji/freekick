ae6c611069a53fe82a95ce04aadb7b495438966d
donkeyji <donkeyji@yahoo.com>
2015-04-21 11:03:26 +0800

    backup improved

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-d=$(date "+%Y-%m-%d")
+d=$(date "+%Y-%m-%d_%H:%M:%S")
 name="hopex-$d.tar.gz"
 dst_dir="/Users/huge/Work/VShared/freekick/code/release-hopex"
 git archive master --prefix='hopex/' | gzip > $dst_dir/$name
