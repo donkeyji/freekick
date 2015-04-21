38d85042105c894af89d9012aba12bb4ba64671b
donkeyji <donkeyji@yahoo.com>
2015-04-21 23:13:26 +0800

    backup.sh

--- backup.sh
+++ backup.sh
@@ -3,8 +3,18 @@
 git push origin master
 d=$(date "+%Y-%m-%d_%H:%M")
 name="hopex-$d.tar.gz"
+
 dst_dir="/Users/huge/Work/VShared/freekick/code/release-hopex"
-git archive master --prefix='hopex/' | gzip > $dst_dir/$name
+mkdir -pv "$dst_dir"
+git archive master --prefix='hopex/' | gzip > "$dst_dir"/$name
+
+dst_dir="/Volumes/SD CARD/X-STARS/release-hopex/"
+mkdir -pv "$dst_dir"
+git archive master --prefix='hopex/' | gzip > "$dst_dir"/$name
+
+dst_dir="/Volumes/NO NAME/Z-Music/release-hopex/"
+mkdir -pv "$dst_dir"
+git archive master --prefix='hopex/' | gzip > "$dst_dir"/$name
 
 bak_dir="/Volumes/SD CARD/X-STARS/hopex/"
 test -d "$bak_dir" && cd "$bak_dir" && git pull
