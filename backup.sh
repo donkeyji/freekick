3ae820d25f7530d8d2f908148d99634674745264
donkeyji <donkeyji@yahoo.com>
2015-04-21 15:41:43 +0800

    backup.sh fixed

--- backup.sh
+++ backup.sh
@@ -6,8 +6,10 @@ name="hopex-$d.tar.gz"
 dst_dir="/Users/huge/Work/VShared/freekick/code/release-hopex"
 git archive master --prefix='hopex/' | gzip > $dst_dir/$name
 
-bak_dirs=("/Volumes/SD CARD/X-STARS/hopex/", "/Volumes/NO NAME/Z-Music/hopex/", "/Volumes/Macintosh/Users/huge/Pictures/hopex/")
-for d in $bak_dir; do
-	test -d "$d" && cd "$d" && git pull
-done
+bak_dir="/Volumes/SD CARD/X-STARS/hopex/"
+test -d "$bak_dir" && cd "$bak_dir" && git pull
+bak_dir="/Volumes/NO NAME/Z-Music/hopex/"
+test -d "$bak_dir" && cd "$bak_dir" && git pull
+bak_dir="/Volumes/Macintosh/Users/huge/Pictures/hopex/"
+test -d "$bak_dir" && cd "$bak_dir" && git pull
 
