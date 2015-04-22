123cc61dee143ff2b14e269fe409932b03712fa9
donkeyji <donkeyji@yahoo.com>
2015-04-22 10:21:36 +0800

    backup.sh

--- backup.sh
+++ backup.sh
@@ -5,20 +5,20 @@ d=$(date "+%Y-%m-%d_%H:%M")
 name="hopex-$d.tar.gz"
 
 dst_dir="/Users/huge/Work/VShared/freekick/code/release-hopex"
-mkdir -pv "$dst_dir"
+test -d "$dst_dir" || mkdir -pv "$dst_dir"
 git archive master --prefix='hopex/' | gzip > "$dst_dir"/$name
 
-dst_dir="/Volumes/SD CARD/X-STARS/release-hopex/"
-mkdir -pv "$dst_dir"
+dst_dir="/Volumes/SDCard/X-STARS/release-hopex"
+#test -d "$dst_dir" || mkdir -pv "$dst_dir"
 git archive master --prefix='hopex/' | gzip > "$dst_dir"/$name
 
-dst_dir="/Volumes/NO NAME/Z-Music/release-hopex/"
-mkdir -pv "$dst_dir"
+dst_dir="/Volumes/Nokia/Z-Music/release-hopex"
+#test -d "$dst_dir" || mkdir -pv "$dst_dir"
 git archive master --prefix='hopex/' | gzip > "$dst_dir"/$name
 
-bak_dir="/Volumes/SD CARD/X-STARS/hopex/"
+bak_dir="/Volumes/SDCard/X-STARS/hopex/"
 test -d "$bak_dir" && cd "$bak_dir" && git pull
-bak_dir="/Volumes/NO NAME/Z-Music/hopex/"
+bak_dir="/Volumes/Nokia/Z-Music/hopex/"
 test -d "$bak_dir" && cd "$bak_dir" && git pull
 bak_dir="/Volumes/Macintosh/Users/huge/Pictures/hopex/"
 test -d "$bak_dir" && cd "$bak_dir" && git pull
