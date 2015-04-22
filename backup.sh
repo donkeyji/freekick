83c7152b0684a28fc2958fdb60947ab81eda4d3e
donkeyji <donkeyji@yahoo.com>
2015-04-22 15:59:19 +0800

    bug fixed for backup.sh

--- backup.sh
+++ backup.sh
@@ -5,16 +5,13 @@ d=$(date "+%Y-%m-%d_%H:%M")
 name="hopex-$d.tar.gz"
 
 dst_dir="/Users/huge/Work/VShared/freekick/code/release-hopex"
-test -d "$dst_dir" || mkdir -pv "$dst_dir"
-git archive master --prefix='hopex/' | gzip > "$dst_dir"/$name
+test -d "$dst_dir" && git archive master --prefix='hopex/' | gzip > "$dst_dir"/$name
 
 dst_dir="/Volumes/SDCard/X-STARS/release-hopex"
-#test -d "$dst_dir" || mkdir -pv "$dst_dir"
-git archive master --prefix='hopex/' | gzip > "$dst_dir"/$name
+test -d "$dst_dir" && git archive master --prefix='hopex/' | gzip > "$dst_dir"/$name
 
 dst_dir="/Volumes/Nokia/Z-Music/release-hopex"
-#test -d "$dst_dir" || mkdir -pv "$dst_dir"
-git archive master --prefix='hopex/' | gzip > "$dst_dir"/$name
+test -d "$dst_dir" && git archive master --prefix='hopex/' | gzip > "$dst_dir"/$name
 
 bak_dir="/Volumes/SDCard/X-STARS/hopex/"
 test -d "$bak_dir" && cd "$bak_dir" && git pull
