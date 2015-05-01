86366b3670d2816a9128982201783292c26076ff
donkeyji <donkeyji@yahoo.com>
2015-05-01 15:16:23 +0800

    rename for fk_item

--- fk_item.c
+++ fk_item.c
@@ -28,12 +28,12 @@ fk_item *fk_item_free_obj_get()
     fk_node *nd;
 
     nd = fk_list_head_pop(free_objs);
-    itm = nd->obj;
+    itm = nd->entity;
     if (itm == NULL) {
         itm = (fk_item *)fk_mem_alloc(sizeof(fk_item)); //really malloc memory here
         itm->type = FK_OBJ_NIL;
         itm->ref = 0;
-        itm->obj = NULL;
+        itm->entity = NULL;
     }
     return itm;
 }
@@ -50,7 +50,7 @@ void fk_item_free_obj_put(fk_item *itm)
 void fk_item_put_free(fk_item *itm)
 {
     itm->type = FK_OBJ_NIL;
-    itm->obj = NULL;
+    itm->entity = NULL;
     itm->ref = 0;
     if (free_objs->len < FK_FREE_OBJS_MAX) {
         fk_list_insert(free_objs, itm);
@@ -60,11 +60,11 @@ void fk_item_put_free(fk_item *itm)
 }
 */
 
-fk_item *fk_item_create(int type, void *obj)
+fk_item *fk_item_create(int type, void *entity)
 {
 	fk_item *itm;
 	itm = (fk_item *)fk_mem_alloc(sizeof(fk_item));
-	itm->obj = obj;
+	itm->entity = entity;
 	itm->ref = 0;
 	itm->type = type;
 
@@ -88,16 +88,16 @@ void fk_item_destroy(fk_item *itm)
 {
 	switch (itm->type) {
 	case FK_ITEM_STR:
-		fk_str_destroy(itm->obj);
+		fk_str_destroy(itm->entity);
 		break;
 	case FK_ITEM_LIST:
-		fk_list_destroy(itm->obj);
+		fk_list_destroy(itm->entity);
 		break;
 	case FK_ITEM_DICT:
-		fk_dict_destroy(itm->obj);
+		fk_dict_destroy(itm->entity);
 		break;
 	}
-	itm->obj = NULL;
+	itm->entity = NULL;
 	itm->ref = 0;
 	itm->type = FK_OBJ_NIL;
 	fk_mem_free(itm);
--- fk_item.h
+++ fk_item.h
@@ -9,17 +9,17 @@
 typedef struct _fk_item {
 	int type;
 	int ref;//reference count
-	void *obj;//point to the real data of the itm
+	void *entity;//point to the real data of the itm
 } fk_item;
 
 void fk_item_init();
-fk_item *fk_item_create(int type, void *obj);
+fk_item *fk_item_create(int type, void *entity);
 void fk_item_ref_dec(fk_item *itm);
 void fk_item_ref_inc(fk_item *itm);
 void fk_item_destroy(fk_item *itm);
 
 #define fk_item_type(itm)	((itm)->type)
 #define fk_item_ref(itm)	((itm)->ref)
-#define fk_item_raw(itm)	((itm)->obj)
+#define fk_item_raw(itm)	((itm)->entity)
 
 #endif
