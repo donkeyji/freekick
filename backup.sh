408afb6401bae1ebc20fdde093cb9fef82cb0bf9
donkeyji <donkeyji@yahoo.com>
2015-05-07 10:51:56 +0800

    backup.sh rename

--- backup.sh
+++ backup.sh
@@ -11,20 +11,20 @@ else
 	d=$(date "+%Y-%m-%d")
 	appendix=$d-$ver
 fi
-name="hopex-$appendix.tar.gz"
+name="freekick-$appendix.tar.gz"
 
-dst_dir="/Users/huge/Work/VShared/freekick/code/release-hopex"
-test -d "$dst_dir" && git archive master --prefix='hopex/' | gzip > "$dst_dir"/$name
+dst_dir="/Users/huge/Work/VShared/freekick/code/release-freekick"
+test -d "$dst_dir" && git archive master --prefix='freekick/' | gzip > "$dst_dir"/$name
 
-dst_dir="/Volumes/SDCard/X-STARS/release-hopex"
-test -d "$dst_dir" && git archive master --prefix='hopex/' | gzip > "$dst_dir"/$name
+dst_dir="/Volumes/SDCard/X-STARS/release-freekick"
+test -d "$dst_dir" && git archive master --prefix='freekick/' | gzip > "$dst_dir"/$name
 
-dst_dir="/Volumes/Nokia/Z-Music/release-hopex"
-test -d "$dst_dir" && git archive master --prefix='hopex/' | gzip > "$dst_dir"/$name
+dst_dir="/Volumes/Nokia/Z-Music/release-freekick"
+test -d "$dst_dir" && git archive master --prefix='freekick/' | gzip > "$dst_dir"/$name
 
-bak_dir="/Volumes/SDCard/X-STARS/hopex/"
+bak_dir="/Volumes/SDCard/X-STARS/freekick/"
 test -d "$bak_dir" && cd "$bak_dir" && git pull
-bak_dir="/Volumes/Nokia/Z-Music/hopex/"
+bak_dir="/Volumes/Nokia/Z-Music/freekick/"
 test -d "$bak_dir" && cd "$bak_dir" && git pull
-bak_dir="/Volumes/Macintosh/Users/huge/Pictures/hopex/"
+bak_dir="/Volumes/Macintosh/Users/huge/Pictures/freekick/"
 test -d "$bak_dir" && cd "$bak_dir" && git pull
