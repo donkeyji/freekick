f43267f1a6762a92954aa9967984cefd652af965
donkeyji <donkeyji@yahoo.com>
2015-05-10 20:08:02 +0800

    formatted

--- fk_vtr.h
+++ fk_vtr.h
@@ -17,27 +17,8 @@ void fk_vtr_destroy(fk_vtr *vtr);
 
 #define fk_vtr_get(vtr, idx)	((vtr)->array)[(idx)]
 
-#define fk_vtr_align(length)	(((length) + FK_VTR_INIT_LEN - 1) & ~(FK_VTR_INIT_LEN - 1))
-
-#define fk_vtr_adjust(vtr, length)	{								\
-	if ((length) > (vtr)->len) {									\
-		(vtr) = (fk_vtr *)fk_mem_realloc((vtr),						\
-				sizeof(fk_vtr) + 									\
-				sizeof(void *) * fk_vtr_align((length)));			\
-		bzero((vtr)->array + (vtr)->len, 							\
-				sizeof(void *) * 									\
-				(fk_vtr_align((length)) - (vtr)->len));				\
-		(vtr)->len = (length);										\
-	} else {														\
-		if ((vtr)->len > FK_VTR_INIT_LEN 							\
-				&& (length) < FK_VTR_INIT_LEN) 						\
-		{															\
-			(vtr) = (fk_vtr *)fk_mem_realloc((vtr), 				\
-				sizeof(fk_vtr) + sizeof(void *) * FK_VTR_INIT_LEN);	\
-			(vtr)->len = FK_VTR_INIT_LEN;							\
-		}															\
-	}																\
-}
+#define fk_vtr_align(length)	\
+	(((length) + FK_VTR_INIT_LEN - 1) & ~(FK_VTR_INIT_LEN - 1))
 
 #define fk_vtr_stretch(vtr, length)		do {					\
 	if ((length) > (vtr)->len) {								\
