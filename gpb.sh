11e14cc0a9045580ad8e8af9dd5951855063bace
donkeyji <donkeyji@yahoo.com>
2016-01-07 18:56:39 +0800

    optimize gpb.sh

--- gpb.sh
+++ gpb.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-all_remotes='
-origin
-github
-bitbucket
-osc
-'
+all_remotes=$(git remote)
+#all_remotes='
+#origin
+#github
+#bitbucket
+#osc
+#'
 
-all_branches='
-v1
-v2
-v3
-v4
-v5
-v6
-master
-'
+all_branches=$(git branch)
+#all_branches='
+#v1
+#v2
+#v3
+#v4
+#v5
+#v6
+#master
+#'
 
 if [ -z $1]; then
 	branches='master'
