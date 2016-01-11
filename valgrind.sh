833e69015d28b54aaf6869720f6231ebffa1e256
root <root@wheezy.gov>
2016-01-11 15:02:14 +0800

    use simple option for valgrind.sh

--- valgrind.sh
+++ valgrind.sh
@@ -2,4 +2,5 @@
 
 test -f ./v.log && rm -f v.log
 
-valgrind --tool=memcheck --track-origins=yes --show-reachable=yes --read-var-info=yes --verbose --time-stamp=yes --leak-check=full --log-file=v.log ./freekick ./freekick.conf
+#valgrind --tool=memcheck --track-origins=yes --show-reachable=yes --read-var-info=yes --verbose --time-stamp=yes --leak-check=full --log-file=v.log ./freekick ./freekick.conf
+valgrind --tool=memcheck --leak-check=full --log-file=v.log ./freekick ./freekick.conf
