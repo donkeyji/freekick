e8dc82a1b7ac3dd8bc3ccdf3e3a8b276d3fe54ff
donkeyji <donkeyji@yahoo.com>
2016-01-17 22:02:23 +0800

    rename high_wat to highwat

--- fk_buf.h
+++ fk_buf.h
@@ -9,14 +9,14 @@
 #define FK_BUF_INIT_LEN 	16
 
 typedef struct _fk_buf {
-	size_t high_wat;
+	size_t highwat;
 	size_t len;
 	size_t low;/* valid begin, init: 0, range: [0-->len], <= high */
 	size_t high;/* free begin, init: 0, range: [0-->len], <= len */
 	char buffer[];
 } fk_buf;
 
-fk_buf *fk_buf_create(size_t high_wat);
+fk_buf *fk_buf_create(size_t highwat);
 void fk_buf_destroy(fk_buf *buf);
 #ifdef FK_DEBUG
 void fk_buf_print(const fk_buf *buf);
@@ -28,7 +28,7 @@ void fk_buf_print(const fk_buf *buf);
 
 #define fk_buf_high(buf)			((buf)->high)
 
-#define fk_buf_high_wat(buf)		((buf)->high_wat)
+#define fk_buf_highwat(buf)		((buf)->highwat)
 
 #define fk_buf_free_len(buf) 		((buf)->len - (buf)->high)
 
@@ -38,7 +38,7 @@ void fk_buf_print(const fk_buf *buf);
 
 #define fk_buf_payload_start(buf) 	((buf)->buffer + (buf)->low)
 
-#define fk_buf_reach_highwat(buf)	((buf)->len == (buf)->high_wat)
+#define fk_buf_reach_highwat(buf)	((buf)->len == (buf)->highwat)
 
 #define fk_buf_high_inc(buf, offset)	{	\
 	(buf)->high += (offset);				\
@@ -55,7 +55,7 @@ void fk_buf_print(const fk_buf *buf);
 }
 
 #define fk_buf_stretch(buf)		{				\
-	if ((buf)->len < (buf)->high_wat) {			\
+	if ((buf)->len < (buf)->highwat) {			\
 		(buf)->len <<= 1;						\
 		(buf) = (fk_buf *)fk_mem_realloc((buf),	\
 				sizeof(fk_buf) + (buf)->len);	\
@@ -90,7 +90,7 @@ void fk_buf_print(const fk_buf *buf);
 		fk_buf_shift((buf));							\
 	}													\
 	if (fk_buf_free_len((buf)) < (length) &&			\
-		(buf)->len < (buf)->high_wat)					\
+		(buf)->len < (buf)->highwat)					\
 	{													\
 		(buf)->len = fk_util_min_power((buf)->len 		\
 			+ (length) - fk_buf_free_len((buf)));		\
