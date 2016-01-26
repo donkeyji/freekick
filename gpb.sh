d2018f3fcf08be3def92f5396735ff405abcac0e
donkeyji <donkeyji@yahoo.com>
2016-01-26 16:27:51 +0800

    optimize gpb.sh

--- gpb.sh
+++ gpb.sh
@@ -2,15 +2,18 @@
 
 all_remotes=$(git remote)
 # the option --no-color is necessary
-all_branches=$(git branch --no-color | sed '/*/s/*//g')
+all_branches=$(git branch --no-color | sed -e '/^  /s/^  //' -e '/^* /s/^* //g')
+echo -e "All Branches:\n$all_branches"
 
 if [ -z $1 ]; then
-	branches='master'
+	branches=$(git branch --no-color|grep '*'|sed '/^* /s/^* //')
 #elif [ $1 = 'all' ]; then
 #	branches=$all_branches
 else
 	branches=$1
 fi
+
+echo -e "Target Branch:\n$branches"
 
 echo "Trying to push all branches to the remote repository..."
 for rt in $all_remotes; do
