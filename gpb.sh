8990e5ff9ed871b73fa1ef5807c4b1de62e5d955
donkeyji <donkeyji@yahoo.com>
2016-01-30 20:01:27 +0800

    add color for echo in gpb.sh

--- gpb.sh
+++ gpb.sh
@@ -3,7 +3,7 @@
 all_remotes=$(git remote)
 # the option --no-color is necessary
 all_branches=$(git branch --no-color | sed -e '/^  /s/^  //' -e '/^* /s/^* //g')
-echo -e "All Branches Available:\n$all_branches"
+echo -e "All Branches Available:\n\033[35m$all_branches\033[0m"
 
 if [ -z $1 ]; then
 	branches=$(git branch --no-color|grep '*'|sed '/^* /s/^* //')
@@ -13,18 +13,18 @@ else
 	branches=$1
 fi
 
-echo -e "Target Branch: $branches"
+echo -e "Target Branch: \033[32m$branches\033[0m"
 read -p "Wanna Continue? [y/n]" choice
 if [ $choice != 'y' ]; then
-	echo "Cancle Pushing"
+	echo -e "\033[33mPushing Canled\033[0m"
 	exit 1
 fi
 
-echo "Trying to push all branches to the remote repository..."
+echo -e "Trying to push all branches to the remote repository..."
 for rt in $all_remotes; do
-	echo "===================Remote Repository: $rt==================="
+	echo -e "===================Remote Repository: \033[31m$rt\033[0m==================="
 	for br in $branches; do
-		echo "---------Local Branch: $br---------"
+		echo -e "---------Local Branch: \033[36m$br\033[0m---------"
 		git push $rt $br
 	done
 done
