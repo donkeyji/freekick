ebbea563c4f94afca6eae8e9382f242a5b64b9ac
donkeyji <donkeyji@yahoo.com>
2016-02-01 20:56:28 +0800

    use (void) for fk_cache

--- fk_cache.c
+++ fk_cache.c
@@ -6,13 +6,13 @@
 /* save free fk_node */
 static fk_free_nodes *free_nodes = NULL;
 
-void fk_cache_init()
+void fk_cache_init(void)
 {
 	free_nodes = fk_rawlist_create(fk_free_nodes);
 	fk_rawlist_init(free_nodes);
 }
 
-fk_node *fk_cache_get_free_node()
+fk_node *fk_cache_get_free_node(void)
 {
 	fk_node *nd;
 
--- fk_cache.h
+++ fk_cache.h
@@ -5,9 +5,9 @@
 
 fk_rawlist_def(fk_node, fk_free_nodes);
 
-void fk_cache_init();
+void fk_cache_init(void);
 
-fk_node *fk_cache_get_free_node();
+fk_node *fk_cache_get_free_node(void);
 void fk_cache_put_free_node(fk_node *nd);
 
 #endif
