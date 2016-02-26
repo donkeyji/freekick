3b752c611d8e021ddbe9632dad57e5b48c544acf
root <root@wheezy.gov>
2016-02-26 18:00:48 +0800

    update astyle.sh

--- astyle.sh
+++ astyle.sh
@@ -2,9 +2,16 @@
 
 files=$1
 if [ -z $files ]; then
-	files='*.c *.h tmod/*.c'
+    files=$(find . -name "*.c" -o -name "*.h")
 fi
+echo files
 
-options='-A10 -s4 -l -Y -m0 -p -H -j -k3 -n'
+indent_options='-di1 -cd1 --no-tabs -c1 -cd1'
+astyle_options='-A10 -s4 -l -Y -m0 -p -H -j -k3 -n'
 
-astyle $options $files
+for f in $files; do
+    #indent $indent_options $f
+    astyle $astyle_options $f
+    #rm -f ${f}.BAK
+    #rm -f ${f}~
+done
