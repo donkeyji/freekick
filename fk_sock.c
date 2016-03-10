5215cd40ec69acb84a5cbc3bcd84634698693eeb
donkeyji <donkeyji@yahoo.com>
2016-03-10 12:31:10 +0800

    use memset to replace bzero in fk_sock.c

--- fk_sock.c
+++ fk_sock.c
@@ -33,7 +33,7 @@ int fk_sock_create_tcp_listen(char *addr, uint16_t port)
         return FK_SOCK_ERR;
     }
 
-    bzero(&saddr, sizeof(struct sockaddr_in));
+    memset(&saddr, 0, sizeof(struct sockaddr_in));
     saddr.sin_port = htons(port);/* uint16, defined in netinet/in.h */
     saddr.sin_family = AF_INET;/* uint8, defined in sys/socket.h */
     //saddr.sin_addr.s_addr = htons(INADDR_ANY);/* uint32, defined in netinet/in.h */
