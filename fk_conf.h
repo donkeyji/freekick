6c1c2298af69ba91f77c2e9a9124af083b4566a7
donkeyji <donkeyji@yahoo.com>
2016-03-12 14:02:04 +0800

    format structs in fk_conf.h

--- fk_conf.h
+++ fk_conf.h
@@ -11,21 +11,21 @@
 #define FK_CONF_ERR		-1
 
 typedef struct {
-    uint16_t port;
-    fk_str_t *addr;
-    int daemon;
-    int max_conns;
-    time_t timeout;
-    int log_level;
-    fk_str_t *log_path;
-    fk_str_t *pid_path;
+    uint16_t   port;
+    fk_str_t  *addr;
+    int        daemon;
+    int        max_conns;
+    time_t     timeout;
+    int        log_level;
+    fk_str_t  *log_path;
+    fk_str_t  *pid_path;
     int dump;
-    fk_str_t *db_file;
-    fk_str_t *dir;
-    uint32_t dbcnt;
-    size_t max_wbuf;
-    int blog_on;
-    fk_str_t *blog_file;
+    fk_str_t  *db_file;
+    fk_str_t  *dir;
+    uint32_t   dbcnt;
+    size_t     max_wbuf;
+    int        blog_on;
+    fk_str_t  *blog_file;
 } fk_conf_t;
 
 extern fk_conf_t setting;
