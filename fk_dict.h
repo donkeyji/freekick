fedf399773fe215d21d083fbd1bf630bc0a8c2fc
donkeyji <donkeyji@yahoo.com>
2016-03-12 14:24:18 +0800

    format structs in fk_dict.h

--- fk_dict.h
+++ fk_dict.h
@@ -9,38 +9,45 @@
 #define FK_DICT_OK		0
 #define FK_DICT_ERR		-1
 
+typedef uint32_t (*fk_key_hash_t)(void *key);/* gen hash */
+typedef int (*fk_key_cmp_t)(void *k1, void *k2);/* key compare */
+typedef void *(*fk_key_copy_t)(void *key);
+typedef void (*fk_key_free_t)(void *key);
+typedef void *(*fk_val_copy_t)(void *val);
+typedef void (*fk_val_free_t)(void *val);
+
 typedef struct {
-    uint32_t (*key_hash)(void *key);/* gen hash */
-    int (*key_cmp)(void *k1, void *k2);/* key compare */
-    void *(*key_copy)(void *key);
-    void (*key_free)(void *key);
-    void *(*val_copy)(void *val);
-    void (*val_free)(void *val);
+    fk_key_hash_t  key_hash;/* gen hash */
+    fk_key_cmp_t   key_cmp;/* key compare */
+    fk_key_copy_t  key_copy;
+    fk_key_free_t  key_free;
+    fk_val_copy_t  val_copy;
+    fk_val_free_t  val_free;
 } fk_elt_op_t;
 
 typedef struct fk_elt_s fk_elt_t;
 struct fk_elt_s {
-    void *key;
-    void *value;
-    fk_elt_t *next;
-    fk_elt_t *prev;
+    void      *key;
+    void      *value;
+    fk_elt_t  *next;
+    fk_elt_t  *prev;
 };
 
 fk_rawlist_def(fk_elt_t, fk_elt_list_t);
 
 typedef struct {
-    size_t size;
-    size_t size_mask;
-    size_t used;
-    size_t limit;
-    fk_elt_list_t **buckets;
-    fk_elt_op_t *eop;
+    size_t           size;
+    size_t           size_mask;
+    size_t           used;
+    size_t           limit;
+    fk_elt_list_t  **buckets;
+    fk_elt_op_t     *eop;
 } fk_dict_t;
 
 typedef struct {
-    fk_dict_t *dct;
-    fk_elt_t *cur, *next;
-    long long idx;/* should be long than size_t type */
+    fk_dict_t  *dct;
+    fk_elt_t   *cur, *next;
+    long long   idx;/* should be long than size_t type */
 } fk_dict_iter_t;
 
 fk_dict_t *fk_dict_create(fk_elt_op_t *eop);
