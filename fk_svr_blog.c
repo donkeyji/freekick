7b8a7153094840422f12ee6a8f1b0b27edb29a57
donkeyji <donkeyji@yahoo.com>
2016-03-12 15:34:30 +0800

    format local variables in fk_svr_blog.c

--- fk_svr_blog.c
+++ fk_svr_blog.c
@@ -19,8 +19,8 @@ fk_blog_load(fk_str_t *blog_path)
 void
 fk_blog_append(int argc, fk_vtr_t *arg_vtr, fk_proto_t *pto)
 {
-    int i;
-    fk_item_t *arg;
+    int         i;
+    fk_item_t  *arg;
 
     if (pto->type == FK_PROTO_READ) {
         return;
