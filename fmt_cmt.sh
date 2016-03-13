5e4eead108caed5f0f91b0588141eb5a5a93c77d
donkeyji <donkeyji@yahoo.com>
2016-03-13 16:21:16 +0800

    integrate fmt_cmt.sh and fmt_cmt2.sh

--- fmt_cmt.sh
+++ fmt_cmt.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-f='*.c *.h'
+files=$*
 
-sed -i ''   '/\/\*[^[:space:]]/s#\/\*\([^[:space:]]\)#/* \1#' $f
+if [ -z $files ]; then
+    files=$(find . -name "*.c" -o -name "*.h")
+fi
 
-sed -i ''   '/[^[:space:]]\*\//s#\([^[:space:]]\)\*\/#\1 */#' $f
+for f in $files; do
+    sed -i ''   '/\/\*[^[:space:]]/s#\/\*\([^[:space:]]\)#/* \1#' $f
+
+    sed -i ''   '/[^[:space:]]\*\//s#\([^[:space:]]\)\*\/#\1 */#' $f
+
+    sed -i ''  '/[^[:space:]]\/\*/s/\([^[:space:]]\)\/\*/\1 \/\*/g'  $f
+done
