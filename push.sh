33007cecf05784684cd1f657fe9214396fe9e838
donkeyji <donkeyji@yahoo.com>
2016-04-18 20:38:29 +0800

    use color definition variable in push.sh

--- push.sh
+++ push.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+# color definition
+COLOR_END="\033[0m"
+COLOR_RED="\033[31m"
+COLOR_GREEN="\033[32m"
+COLOR_YELLOW="\033[33m"
+COLOR_BLUE="\033[34m"
+COLOR_PURPLE="\033[35m"
+COLOR_CYAN="\033[36m"
+COLOR_WHITE="\033[37m"
+
 all_remotes=$(git remote)
 # the option --no-color is necessary
 all_branches=$(git branch --no-color | sed -e '/^  /s/^  //' -e '/^* /s/^* //g')
-echo -e "All Branches Available:\n\033[35m$all_branches\033[0m"
+echo -e "All Branches Available:\n${COLOR_PURPLE}${all_branches}${COLOR_END}"
 
 if [ -z $1 ]; then
     branches=$(git branch --no-color|grep '*'|sed '/^* /s/^* //')
@@ -13,18 +23,18 @@ else
     branches=$1
 fi
 
-echo -e "Target Branch: \033[32m$branches\033[0m"
+echo -e "Target Branch: ${COLOR_GREEN}$branches${COLOR_END}"
 read -p "Wanna Continue? [y/n]: " choice
 if [ $choice != 'y' ]; then
-    echo -e "\033[33mPushing Canled\033[0m"
+    echo -e "${COLOR_YELLOW}Pushing Canled${COLOR_END}"
     exit 1
 fi
 
 echo -e "Trying to push all branches to the remote repository..."
 for rt in $all_remotes; do
-    echo -e "===================Remote Repository: \033[31m$rt\033[0m==================="
+    echo -e "===================Remote Repository: ${COLOR_RED}$rt${COLOR_END}==================="
     for br in $branches; do
-        echo -e "---------Local Branch: \033[36m$br\033[0m---------"
+        echo -e "---------Local Branch: ${COLOR_CYAN}$br${COLOR_END}--------"
         git push $rt $br
     done
 done
