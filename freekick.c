a881b9ab052a7271146629fd74f9b16abcf452e0
donkeyji <donkeyji@yahoo.com>
2016-06-14 15:52:39 +0800

    update the comment for fk_daemon_run_old

--- freekick.c
+++ freekick.c
@@ -43,7 +43,7 @@ static void fk_main_cycle(void);
 static void fk_main_exit(void);
 
 /*
- * daemon() is obsolete
+ * just because this daemon() library function is obsolete
 static void
 fk_daemon_run_old(void)
 {
