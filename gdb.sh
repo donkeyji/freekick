b226b25594081fbb6ce0865c4990a6089f46f774
donkeyji <donkeyji@yahoo.com>
2016-08-23 16:13:01 +0800

    update gdb.sh

--- gdb.sh
+++ gdb.sh
@@ -5,9 +5,10 @@ ulimit -c unlimited
 
 # the core file's name is determined by /proc/sys/kernel/core_pattern
 # the default names vary across ditributions
-CORE_FILE=/tmp/core*
+CORE_FILE=/tmp/*core*
 FK_CONF=freekick.conf
 FK_BIN=freekick
+
 if [ -f $CORE_FILE ]; then
     gdb -c $CORE_FILE --args $FK_BIN $FK_CONF
 else
