df447c6372987fb74510bb913f15505535f2f753
donkeyji <donkeyji@yahoo.com>
2016-10-10 14:55:09 +0800

    refine t.sh, adding a repeat_cnt argument

--- t/t.sh
+++ t/t.sh
@@ -1,7 +1,15 @@
 #/bin/bash
 
+repeat_cnt=$1
+
+if [ -z $repeat_cnt ]; then
+    repeat_cnt=1
+fi
+
 echo "testing lua script"
 redis-cli --eval t.lua a b , x y
 
-echo "testing other commands"
-python t.py
+for ((i=0;i<$repeat_cnt;i++)) {
+    echo "testing other commands"
+    python t.py
+}
